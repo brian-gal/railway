f7b5f62cea8c792a9a76a9af876d2a6fd118f07a
Brian <briamgalvez2013@gmail.com>
2026-01-21 16:06:26 -0300

    prueba correcion

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql2/promise");
 
+const database =
+    process.env.MYSQLDATABASE ||
+    process.env.MYSQL_DATABASE;
+
 const pool = mysql.createPool({
+    uri: process.env.MYSQL_URL || process.env.DATABASE_URL,
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    database,
     port: Number(process.env.MYSQLPORT),
     waitForConnections: true,
     connectionLimit: 10,
