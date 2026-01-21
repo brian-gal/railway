2d6c6dadecd879e0d46190b766dc2dde4696311e
Brian <briamgalvez2013@gmail.com>
2026-01-21 15:32:08 -0300

    eliminar node_modules

--- index.js
+++ index.js
@@ -18,35 +18,6 @@ app.get("/", (req, res) => {
     });
 });
 
-app.get("/db-test", async (req, res) => {
-    try {
-        const [rows] = await pool.query("SELECT 1 + 1 AS result");
-        res.json({
-            status: "ok",
-            result: rows[0].result
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "MySQL connection failed" });
-    }
-});
-
-
-app.post("/portal-users", async (req, res) => {
-    try {
-        const name = req.body?.name || "Nombre Predeterminado";
-        const result = await pool.query(
-            "INSERT INTO portal_users (name) VALUES (?)",
-            [name]
-        );
-        res.json({ status: "ok", id: result[0].insertId });
-    } catch (error) {
-        res.status(500).json({ error: "Error al guardar el nombre." });
-    }
-});
-
-
-
 // Arranque del servidor
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en puerto ${PORT}`);
