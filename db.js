cac8bd8458bcfe0721d5b8fd3f9b9c6ef3f5849c
Brian <briamgalvez2013@gmail.com>
2026-01-21 17:00:34 -0300

    3

--- db.js
+++ db.js
@@ -1,3 +1,11 @@
-const mysql = require("mysql2/promise");
-const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
-module.exports = pool;
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
+});
+
+export default pool;
